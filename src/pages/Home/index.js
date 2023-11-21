7553f799c47229077887213907ca916348ac021e
MarcosWinther <winthermarcos@gmail.com>
2023-11-21 11:57:13 -0300

    Criando aplicação GitFind parte 2

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,21 @@
 import { Header } from "../../components/Header";
+import background from '../../assets/background.png';
+
+import './styles.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <div className="content">
+        <img src={background} className="background" alt="background app" />
+        <div className="info">
+          <div>
+            <input name="user" placeholder="@user" />
+            <button>Buscas</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
