ab415fef1dc7123bc22c76c5e23c346b3893b431
MarcosWinther <winthermarcos@gmail.com>
2023-11-21 21:38:16 -0300

    Criando aplicação GitFind parte 3

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -14,6 +14,15 @@ function App() {
             <input name="user" placeholder="@user" />
             <button>Buscas</button>
           </div>
+          <div className="profile_info">
+            <img src="https://avatars.githubusercontent.com/u/44624583?v=4" className="profile" alt="Foto perfil Marcos Winther" />
+            <div>
+              <h3>Marcos Winther</h3>
+              <span>@marcossilvacarreiro</span>
+              <p>Descrição</p>
+            </div>
+          </div>
+          <hr />
         </div>
       </div>
     </div>
